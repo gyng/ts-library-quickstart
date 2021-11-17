eb505d5efd14256e0e9ee0e985f357c9752e0525
Ng Guoyou <guoyou@basis-ai.com>
2021-11-17 17:06:15 +0800

    feat: Generate API docs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
 export const HELLO = "hello";
+
+/**
+ * Example interface
+ */
+export interface IFoo {
+  bar: string;
+}
+
+/**
+ *
+ * @param val Example parameter
+ * @returns An example return object.
+ *
+ * ```ts
+ * const exampleResult = getFoo("bar");
+ * ```
+ */
+export const getFoo = (val: string): IFoo => ({ bar: val });
+
+/**
+ * Example class.
+ */
+export class MyClass {
+  foo: string;
+
+  /**
+   * Example constructor.
+   * @param foo Example constructor param.
+   */
+  constructor(foo: string) {
+    this.foo = foo;
+  }
+
+  /**
+   * Example method.
+   * @returns A greeting.
+   */
+  public hello(): string {
+    return HELLO;
+  }
+}
